3c64a659bee2bf393ab4b34614e52a40fe01f52c
rosalierazonable <rsrazonable1@up.edu.ph>
2021-04-21 02:05:34 +0800

    remove final keyword for request_count attribute

--- src/com/rosalieraz/cmsc125/User.java
+++ src/com/rosalieraz/cmsc125/User.java
@@ -3,7 +3,7 @@ package com.rosalieraz.cmsc125;
 public class User {
 
     private final int id; //user 1, 2, 3 .. etc
-    private final int request_count; //randomized number of request to have
+    private int request_count; //randomized number of request to have
 //    Request[] reqs = new Request[request_count];
 
     User(int id, int request_count) { //Constructor
@@ -18,5 +18,8 @@ public class User {
     int getId(){return this.id;}
     int getRequestCount() {
         return this.request_count;
+    }
+    void updateRequestCount() {
+        this.request_count--;
     }
 }
