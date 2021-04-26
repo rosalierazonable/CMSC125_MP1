b7f4e371161443561678c38cffa8cd17ad8e1f45
rosalierazonable <rsrazonable1@up.edu.ph>
2021-04-26 18:00:23 +0800

    refactor user class

--- src/com/rosalieraz/cmsc125/User.java
+++ src/com/rosalieraz/cmsc125/User.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 public class User {
 
     private final int id; //user 1, 2, 3 .. etc
-//    Request[] userRequests;
     ArrayList<Request> userRequests;
 
     User(int id) { //Constructor
@@ -15,20 +14,15 @@ public class User {
 
     //Getter Functions
     int getId(){return this.id;}
-//    int getRequestCount() {return this.userRequests.length;}
     int getRequestCount() {return this.userRequests.size();}
 
     //Setter Functions
-//    void setUserRequests(Request[] requests) {
-//        this.userRequests = requests;
-//    }
     void setUserRequests(ArrayList<Request> requests) {
         this.userRequests = requests;
     }
 
     //Helper functions
     void displayUserDetail() {
-//        System.out.println("User " + this.id + " has " + this.userRequests.length + " resource request/s.");
         System.out.println("User " + this.id + " has " + this.userRequests.size() + " resource request/s.");
     }
 
